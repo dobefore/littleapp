9bc8f62cc73bce59c7c6823432126e5c1c515111
dobefore <1432338032@qq.com>
2024-02-09 19:41:57 +0800

    fix no createtime on android

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@ use std::fs;
 use std::io;
 use std::path::PathBuf;
 use std::time::SystemTime;
-
+fn split(t:&str) ->String{
+    let v=t.split('_').collect::<Vec<&str>>();
+   let f= v.get(2).unwrap().to_string();
+   f
+}
 fn main() -> io::Result<()> {
     let args:Vec<String>=env::args().collect();
     let p=args.get(1).unwrap();
@@ -20,11 +24,11 @@ fn main() -> io::Result<()> {
     
     // 根据文件的创建时间对文件进行排序
     files.sort_by(|a, b| {
-        let metadata_a = fs::metadata(a).unwrap();
-        let metadata_b = fs::metadata(b).unwrap();
-        let created_at_a = metadata_a.created().unwrap();
-        let created_at_b = metadata_b.created().unwrap();
-        created_at_a.cmp(&created_at_b)
+        let metadata_a = a.file_stem().unwrap();
+        let metadata_b = b.file_stem().unwrap();
+        let created_at_a = split(&metadata_a.to_str().unwrap()).parse::<i32>();
+        let created_at_b = split(&metadata_b.to_str().unwrap()).parse::<i32>();
+        created_at_a.unwrap().cmp(&created_at_b.unwrap())
     });
     
     // 重命名文件
