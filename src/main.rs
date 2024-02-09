629c78a35a43e491645fa5092792a383531aea5b
dobefore <1432338032@qq.com>
2024-02-09 14:23:27 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::fs;
+use std::io;
+use std::path::PathBuf;
+use std::time::SystemTime;
+
+fn main() -> io::Result<()> {
+    // 获取当前工作目录
+    let current_dir = std::env::current_dir()?;
+    
+    // 读取当前目录下的所有文件
+    let mut files = fs::read_dir(current_dir)?
+        .filter(|res| res.is_ok())
+.filter(|entry| { let path = entry.clone();
+   let path=path .as_ref().unwrap().path(); path.extension().and_then(|ext| ext.to_str()) == Some("mp4") }) 
+        .map(|res| res.unwrap().path())
+        .collect::<Vec<PathBuf>>();
+    
+    // 根据文件的创建时间对文件进行排序
+    files.sort_by(|a, b| {
+        let metadata_a = fs::metadata(a).unwrap();
+        let metadata_b = fs::metadata(b).unwrap();
+        let created_at_a = metadata_a.created().unwrap();
+        let created_at_b = metadata_b.created().unwrap();
+        created_at_a.cmp(&created_at_b)
+    });
+    
+    // 重命名文件
+    for (index, file) in files.iter().enumerate() {
+        let new_name = format!("{}.mp4", index + 1);
+        fs::rename(file, file.with_file_name(new_name))?;
+    }
+    
+    Ok(())
+}
+
+ 
