2216781f76ce9f6a775513d7ac2e20941f3aae08
dobefore <1432338032@qq.com>
2024-02-09 18:58:22 +0800

    read path from command line

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
+use std::env;
 use std::fs;
 use std::io;
 use std::path::PathBuf;
 use std::time::SystemTime;
 
 fn main() -> io::Result<()> {
+    let args:Vec<String>=env::args().collect();
+    let p=args.get(1).unwrap();
     // 获取当前工作目录
-    let current_dir = std::env::current_dir()?;
+    let dir = std::path::Path::new(p);
     
     // 读取当前目录下的所有文件
-    let mut files = fs::read_dir(current_dir)?
+    let mut files = fs::read_dir(dir)?
         .filter(|res| res.is_ok())
 .filter(|entry| { let path = entry.clone();
    let path=path .as_ref().unwrap().path(); path.extension().and_then(|ext| ext.to_str()) == Some("mp4") }) 
